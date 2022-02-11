427780d840eacb66df983fa96e85af7deb5d01e1
Яков Панов <diegoling33@yandex.ru>
2022-02-11 06:21:00 +0300

    добавлено отображение по /

--- src/main/java/com/metaprofile/api/security/WebSecurityConfig.java
+++ src/main/java/com/metaprofile/api/security/WebSecurityConfig.java
@@ -57,6 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests().antMatchers("/api/v1/auth/**").permitAll()
                 .antMatchers("/api/v1/user/**").permitAll()
                 .antMatchers("/api/v1/file/**").permitAll()
+                .antMatchers("/").permitAll()
                 .anyRequest().authenticated();
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
