fceb77370b01922ef5b53d1c63066c000da8b3df
Яков Панов <diegoling33@yandex.ru>
2022-02-11 05:54:42 +0300

    тест deploy команды

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,34 @@
  #!/bin/bash
-APP=meta-profile-api
+APP=metaprofile
 PORT=3010
+DOCKER_USER=notafoks
 
 echo '[1/3] Removing the container...'
 docker ps -q --filter "name=${APP}" | grep -q . && docker stop ${APP}
 docker rm -fv ${APP}
 
-echo '[2/3] Building the container...'
-docker build -t diegoling/${APP} .
-
 echo '[3/3] Running the container...'
-docker run --name ${APP} -p ${PORT}:8080 -d diegoling/$APP
+docker login -u ${DOCKER_USER} -p v3Ttris49452
+docker run --name ${APP} -p ${PORT}:8080 -d ${DOCKER_USER}/${APP}:latest
 
-
+#
+###!/bin/bash
+##set -e
+#
+#SERVICES=$(sudo docker service ls --filter name=${APP} --quiet | wc -l)
+#
+#if [[ "$SERVICES" -gt 0 ]]; then
+#    # Update
+#    sudo docker service update \
+#        --detach=false \
+#        --image notafoks/${APP}:latest \
+#        ${APP}
+#
+#else
+#    # Create
+#    sudo docker service create \
+#        --name=$SERVICE_NAME \
+#        --replicas 1 \
+#        --publish ${PORT}:8080 \
+#        notafoks/${APP}:latest
+#fi
