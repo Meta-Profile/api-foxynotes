ca45d862aaa4075a8098813f94161d87d8333867
Яков Панов <diegoling33@yandex.ru>
2022-02-13 04:05:46 +0300

    ошибка авторизации теперь отображается корректно

--- src/main/java/com/metaprofile/api/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/metaprofile/api/security/jwt/AuthEntryPointJwt.java
@@ -1,14 +1,19 @@
 package com.metaprofile.api.security.jwt;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+/**
+ * Обработчик ошибки авторизации
+ */
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
@@ -17,6 +22,10 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
             throws IOException {
         logger.error("Unauthorized error: {}", authException.getMessage());
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getOutputStream().println("{ \"error\": \"" + authException.getMessage() + "\", \"response\": null, \"status\": 401 }");
     }
+
 }
