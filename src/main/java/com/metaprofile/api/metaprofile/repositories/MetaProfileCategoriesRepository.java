4442daa050c87c5d893ccb601aa158b128dd3f93
Яков Панов <diegoling33@yandex.ru>
2022-03-05 04:01:54 +0300

    added repository sorting

--- src/main/java/com/metaprofile/api/metaprofile/repositories/MetaProfileCategoriesRepository.java
+++ src/main/java/com/metaprofile/api/metaprofile/repositories/MetaProfileCategoriesRepository.java
@@ -10,7 +10,16 @@ import java.util.List;
 public interface MetaProfileCategoriesRepository extends JpaRepository<MetaProfileCategory, Long> {
     @Query(
             nativeQuery = true,
-            value = "SELECT * from meta_profile_categories WHERE upper(title ->> :lang) like upper('%'||:query||'%') LIMIT 20"
+            value = "SELECT * from meta_profile_categories " +
+                    "WHERE upper(title ->> :lang) like upper('%'||:query||'%') " +
+                    "ORDER BY" +
+                    "  CASE" +
+                    "    WHEN upper(title ->> :lang) LIKE upper(:query) THEN 1" +
+                    "    WHEN upper(title ->> :lang) LIKE upper(:query||'%') THEN 2" +
+                    "    WHEN upper(title ->> :lang) LIKE upper('%'||:query) THEN 4" +
+                    "    ELSE 3" +
+                    "  END," +
+                    "title ->> :lang ASC LIMIT 20 "
     )
     List<MetaProfileCategory> findByQuery(@Param("query") String query, @Param("lang") String lang);
 
--- src/main/java/com/metaprofile/api/metaprofile/repositories/MetaProfileFieldsRepository.java
+++ src/main/java/com/metaprofile/api/metaprofile/repositories/MetaProfileFieldsRepository.java
@@ -13,7 +13,14 @@ public interface MetaProfileFieldsRepository extends JpaRepository<MetaProfileFi
             value = "SELECT * from meta_profile_fields " +
                     "WHERE mpc_id = :mpcId " +
                     "and upper(title ->> :lang) like upper('%'||:query||'%') " +
-                    "LIMIT 20"
+                    "ORDER BY" +
+                    "  CASE" +
+                    "    WHEN upper(title ->> :lang) LIKE upper(:query) THEN 1" +
+                    "    WHEN upper(title ->> :lang) LIKE upper(:query||'%') THEN 2" +
+                    "    WHEN upper(title ->> :lang) LIKE upper('%'||:query) THEN 4" +
+                    "    ELSE 3" +
+                    "  END," +
+                    "title ->> :lang LIMIT 20 "
     )
     List<MetaProfileField> findByQuery(@Param("query") String query, @Param("mpcId") Long mpcId, @Param("lang") String lang);
 
